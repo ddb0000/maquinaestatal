d723d02280ca647024ca2d5851cbca2ab8174f07
dan <112875547+ddb0000@users.noreply.github.com>
2024-03-05 15:15:43 +0000

    seeker

--- buscador.py
+++ buscador.py
@@ -1,4 +1,5 @@
-# scraper that gets data from the web and stores it in a file, using selenium to blaze through the pages and pandas to store the data in a csv file.
+# scraper that gets data from the web
+# throws ready to machine in endpoint?
 import pandas as pd
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
