97d63596e767b2c35d9ccb2bcfaefef1c9eb622c
dan <112875547+ddb0000@users.noreply.github.com>
2024-03-05 15:28:58 +0000

    0503241528

--- buscador.py
+++ buscador.py
@@ -2,8 +2,29 @@
 # throws ready to machine in endpoint?
 import pandas as pd
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 import time
-import os
-import logging
+
+def init_driver(headless=True):
+    options = Options()
+    if headless:
+        options.add_argument('--headless')
+    driver = webdriver.Chrome(options=options)
+    return driver
+
+def scrape_data(driver, url):
+    driver.get(url)
+    time.sleep(2)
+
+def main():
+    driver = init_driver()
+    urls = [
+    'https://www.olx.com.br/imoveis/estado-rj/rio-de-janeiro-e-regiao/niteroi'
+    ]
+    for url in urls:
+        scrape_data(driver, url)
+    driver.quit()
+
+if __name__ == '__main__':
+    main()
