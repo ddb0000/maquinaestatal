a4cdb8b3f3934e142b6c92b77599d7b5aad27914
Daniel Barcellos <dbarcelloz@gmail.com>
2024-03-05 20:12:20 -0300

    miniseeker

--- buscador.py
+++ buscador.py
@@ -8,38 +8,36 @@ def init_driver(headless=True):
     options = Options()
     if headless:
         options.add_argument('--headless')
-    options.add_argument('--no-sandbox')  # Bypass OS security model, required for Docker
-    options.add_argument('--disable-dev-shm-usage')  # Overcome limited resource problems
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-dev-shm-usage')
     driver = webdriver.Chrome(options=options)
     return driver
 
 def scrape_data(driver, url):
-    driver.get(url)
-    wait = WebDriverWait(driver, 10)
-    # Wait for the specific element to ensure the page has loaded
-    listings = wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, 'section[data-ds-component="DS-AdCard"]')))
-    for listing in listings:
-        # Get the title of the listing
-        try:
-            title = listing.find_element(By.CSS_SELECTOR, 'h2[class*="olx-ad-card__title"]').text
-        except Exception as e:
-            title = "No Title"
-            print(f"Error getting title: {e}")
-        
-        # Get the price of the listing
-        try:
-            price = listing.find_element(By.CSS_SELECTOR, 'h3[class*="olx-ad-card__price"]').text
-        except Exception as e:
-            price = "No Price"
-            print(f"Error getting price: {e}")
-        
-        print(f"{title} - {price}")
+    try:
+        driver.get(url)
+        wait = WebDriverWait(driver, 10)
+        listings = wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, 'section[data-ds-component="DS-AdCard"]')))
+        for listing in listings:
+            try:
+                title = listing.find_element(By.CSS_SELECTOR, 'h2[class*="olx-ad-card__title"]').text
+                price = listing.find_element(By.CSS_SELECTOR, 'h3[class*="olx-ad-card__price"]').text
+                print(f"{title} - {price}")
+            except Exception as e:
+                print(f"Error in listing extraction: {e}")
+    except Exception as e:
+        print(f"An error occurred during scraping: {e}")
 
 def main():
-    driver = init_driver(headless=False)  # Set headless=False to see what the browser is doing
-    url = 'https://www.olx.com.br/imoveis/estado-rj/rio-de-janeiro-e-regiao/niteroi'
-    scrape_data(driver, url)
-    driver.quit()
+    driver = init_driver(headless=False)
+    url = 'https://www.olx.com.br/imoveis/estado-rj/rio-de-janeiro-e-regiao/niteroi?sf=1'
+    try:
+        scrape_data(driver, url)
+    except KeyboardInterrupt:
+        print("Script interrupted by user.")
+    finally:
+        driver.quit()
+        print("Driver closed.")
 
 if __name__ == '__main__':
     main()
